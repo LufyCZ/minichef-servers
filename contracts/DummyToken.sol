2f2637921a1f8f2f37960c19a688150d94fe321c
Matthew Lilley <hello@matthewlilley.com>
2021-12-16 17:01:43 +0000

    fix: 1 unit and 0 dp

--- contracts/DummyToken.sol
+++ contracts/DummyToken.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract DummyToken is ERC20 {
-  constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+   constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
     _mint(msg.sender, 1);
   }
 
   function decimals() public pure override returns (uint8) {
-    return 1;
+    return 0;
   }
 }
